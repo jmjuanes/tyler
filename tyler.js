97fec26526a939fcbd9600afe3a4c285249377c0
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-10-31 20:43:30 +0100

    feat: allow adding custom markers (#2)

--- tyler.js
+++ tyler.js
@@ -6,9 +6,6 @@ const clamp = (value, minValue, maxValue) => {
     return Math.min(maxValue, Math.max(minValue, value));
 };
 
-// Calculate the number of tiles for the given zoom level
-const numTiles = z => Math.pow(2, z);
-
 const lon2tile = lon => {
     return (lon + 180) / 360;
 };
@@ -16,11 +13,6 @@ const lon2tile = lon => {
 const lat2tile = lat => {
     const radians = lat * Math.PI / 180;
     return (1 - Math.log(Math.tan(radians) + 1 / Math.cos(radians)) / Math.PI) / 2;
-};
-
-const latlon2xy = (lat, lon, z) => {
-    const n = numTiles(z);
-    return [n*lon2tile(lon), n*lat2tile(lat)];
 };
 
 const getTileUrl = (url, x, y, z) => {
@@ -45,10 +37,35 @@ const loadImageAsync = imageUrl => {
     });
 };
 
+const markerColors = {
+    default: ["#35889E", "#3BB2D0"],
+};
+
+const getMarkerImage = (color = "default", size = 48) => {
+    const c = markerColors[color];
+    const image = document.createElement("img");
+    const svg = [
+        `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">`,
+        `<g strokeLinecap="round" strokeLinejoin="round" strokeWidth="1">`,
+        `<path fill="${c[1]}" stroke="${c[0]}" d="M12 3C5 3 5 10 5 10C5 16 12 21 12 21C12 21 19 16 19 10C19 10 19 3 12 3Z" />`,
+        `<path fill="#fff" stroke="${c[0]}" d="M9 10C9 8.34315 10.3431 7 12 7C13.6569 7 15 8.34315 15 10C15 11.6569 13.6569 13 12 13C10.3431 13 9 11.6569 9 10Z" />`,
+        `</g>`,
+        `</svg>`,
+    ].join("");
+    const svgBlob = new Blob([svg], {
+        type: "image/svg+xml;charset=utf-8",
+    });
+    image.src = (window.URL || window.webkitURL || window).createObjectURL(svgBlob);
+    image.setAttribute("width", `${size}px`);
+    image.setAttribute("height", `${size}px`);
+    return image;
+};
+
 const getMapTemplate = () => {
     const templateContent = [
         `<div class="tyler">`,
         `    <canvas class="tyler-canvas" width="0" height="0"></canvas>`,
+        `    <div class="tyler-marks"></div>`,
         `    <div class="tyler-attribution"></div>`,
         `    <div class="tyler-zooming" style="display:none;">`,
         `        <div class="tyler-zooming-button zoom-in">+</div>`,
@@ -72,21 +89,24 @@ export const create = (parent, options = {}) => {
     const zooming = options?.zooming ?? true;
     const state = {
         ready: false,
+        x: lon2tile(options?.center?.[1] ?? 0),
+        y: lat2tile(options?.center?.[0] ?? 0),
         zoom: clamp(options?.zoom ?? 10, minZoom, maxZoom),
+        marks: Array.isArray(options?.marks) ? options.marks : [],
     };
     parent.appendChild(getMapTemplate());
     parent.querySelector(".tyler").style.width = options?.width || "100%";
     parent.querySelector(".tyler").style.height = options?.height || "400px";
     parent.querySelector(".tyler-attribution").innerHTML = attribution;
-    const canvas = parent.querySelector("canvas");
-
+    const canvas = parent.querySelector(`canvas`);
     const resize = () => {
-        const {width, height} = canvas.parentElement.getBoundingClientRect();
-        canvas.width = width;
-        canvas.height = height;
+        const size = canvas.parentElement.getBoundingClientRect();
+        canvas.width = size.width;
+        canvas.height = size.height;
     };
-
-    const render = async (center) => {
+    const render = async () => {
+        const numTiles = Math.pow(2, state.zoom);
+        const xAbsolute = state.x * numTiles, yAbsolute = state.y * numTiles;
         const {width, height} = canvas;
         const context = canvas.getContext("2d");
 
@@ -96,36 +116,38 @@ export const create = (parent, options = {}) => {
         const xRange = [-parseInt(Math.floor(tilesX / 2)), parseInt(Math.ceil(tilesX / 2))];
         const yRange = [-parseInt(Math.floor(tilesY / 2)), parseInt(Math.ceil(tilesY / 2))];
 
-        const [xAbsolute, yAbsolute] = latlon2xy(center[0], center[1], state.zoom);
         const xOffset = ((tilesX * tileWidth) - width) / 2;
         const yOffset = ((tilesY * tileHeight) - height) / 2;
 
-        const xCenterDiff = parseInt((xAbsolute - parseInt(xAbsolute)) * tileWidth);
-        const yCenterDiff = parseInt((yAbsolute - parseInt(yAbsolute)) * tileHeight);
+        const xCenterDiff = (xAbsolute - parseInt(xAbsolute)) * tileWidth;
+        const yCenterDiff = (yAbsolute - parseInt(yAbsolute)) * tileHeight;
 
-        // Generate tiles list
-        const tiles = [];
+        // We need first tile to place the other tiles in the correct position
+        const firstTile = [parseInt(xAbsolute) + xRange[0], parseInt(yAbsolute) + yRange[0]];
+        // Render marks
+        state.marks.forEach((mark, index) => {
+            const x = lon2tile(mark.position[1]) * numTiles, y = lat2tile(mark.position[0]) * numTiles;
+            const centerX = ((x - firstTile[0]) * tileWidth) - xOffset - xCenterDiff;
+            const centerY = ((y - firstTile[1]) * tileHeight) - yOffset - yCenterDiff;
+            const markImage = document.querySelector(`img[data-role="marker"][data-index="${index}"]`);
+            markImage.setAttribute("data-visible", (0 < centerX && centerX < width && 0 < centerY && centerY < height) ? "true" : "false");
+            markImage.style.top = `${clamp(centerY, 0, height)}px`;
+            markImage.style.left = `${clamp(centerX, 0, width)}px`;
+        });
+        // Render tiles
         for (let y = yRange[0]; y < yRange[1]; y++) {
             for (let x = xRange[0]; x < xRange[1]; x++) {
-                tiles.push([parseInt(xAbsolute) + x, parseInt(yAbsolute) + y]);
-            }
-        }
-
-        // Render tiles
-        const firstTile = tiles[0];
-        await Promise.all(tiles.map(tile => {
-            const imageUrl = getTileUrl(tileUrl, tile[0], tile[1], state.zoom);
-            return loadImageAsync(imageUrl).then(image => {
+                const tile = [parseInt(xAbsolute) + x, parseInt(yAbsolute) + y];
+                const imageUrl = getTileUrl(tileUrl, tile[0], tile[1], state.zoom);
+                const image = await loadImageAsync(imageUrl);
                 const imageX = ((tile[0] - firstTile[0]) * tileWidth) - xOffset - xCenterDiff;
                 const imageY = ((tile[1] - firstTile[1]) * tileHeight) - yOffset - yCenterDiff;
                 context.drawImage(image, imageX, imageY, tileWidth, tileHeight);
-            });
-        }));
-
+            }
+        }
         // Render finished
         state.ready = true;
     };
-
     // Enable zooming
     if (zooming) {
         const changeZoom = newZoom => {
@@ -142,9 +164,18 @@ export const create = (parent, options = {}) => {
             return state.ready && changeZoom(state.zoom - 1);
         });
     }
-
+    // Render marks
+    state.marks.forEach((mark, index) => {
+        const markImage = getMarkerImage();
+        markImage.setAttribute("data-role", "marker");
+        markImage.setAttribute("data-index", index);
+        markImage.setAttribute("data-latitude", mark.position[0]);
+        markImage.setAttribute("data-longitude", mark.position[1]);
+        markImage.setAttribute("data-visible", "false");
+        parent.querySelector(".tyler-marks").appendChild(markImage);
+    });
     resize();
-    render(options?.center ?? [10, 10]);
+    render();
 
     return {};
 };
